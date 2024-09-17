724527ab0b19b0b66082a664ebe4e77f432588e7
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-17 20:33:41 -0300

    feat: schema for language created.

---
+++ backend/src/infrastructure/schemas/user/languageSchema.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+
+interface ILanguage {
+    _id: string;
+    name: string;
+}
+
+const languageSchema = new Schema({
+    _id: { type: String },
+    name: { type: String },
+});
+
+const LanguageModel = model<ILanguage>("Language", languageSchema);
+
+export { ILanguage, LanguageModel };
