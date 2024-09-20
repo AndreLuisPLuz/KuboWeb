a6b6eadfdaf3900c8af81c445cff847f4b0543e9
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-20 08:51:34 -0300

    feat: Footer component created.

---
+++ frontend/src/Components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { Foot, FooterItemsContainer } from "./style";
+
+const Footer = (): ReactNode => {
+    return (
+        <Foot>
+            <FooterItemsContainer>
+
+            </FooterItemsContainer>
+        </Foot>
+    );
+};
+
+export default Footer;
