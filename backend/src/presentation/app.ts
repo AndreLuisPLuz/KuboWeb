2f9fd66230578733630a2a7c4e7f8def4e3704e4
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-23 08:48:58 -0300

    feat: set up POST /auth route.

--- backend/src/presentation/app.ts
+++ backend/src/presentation/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
+import authRouter from "./routers/authRouter";
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,7 @@ if (process.env.NODE_ENV == "development") {
     app.use(cors({ origin: "*" }));
     console.log("[server]: using cors");
 }
+
+app.use("/api/v1/auth", authRouter);
 
 export default app;
