0138af5490361f13a92427450fe967d423ef1afc
MuryloSaladino <murylosaladino@gmail.com>
2024-09-26 15:56:12 -0300

    feat: added method to find any given number of entities based on user criteria to the generic repository contract.

--- backend/src/domain/seed/repository.ts
+++ backend/src/domain/seed/repository.ts
@@ -1,8 +1,21 @@
 import Entity from "./entity";
 
+type Criteria<TType extends Object, TKey extends keyof TType = keyof TType> = {
+    key: TKey;
+    value: TType[TKey];
+};
+
 interface IRepository<TEntity extends Entity<any>> {
     existsAsync: (id: string) => Promise<boolean>;
-    findByIdAsync:  (id: string) => Promise<TEntity | null>;
+
+    findByIdAsync: (id: string) => Promise<TEntity | null>;
+
+    findManyByCriteriaAsync: (
+            criteria: Criteria<TEntity>,
+            offset: number | null,
+            take: number | null
+    ) => Promise<TEntity[]>;
+    
     upsertAsync: (entity: TEntity) => Promise<TEntity | null>;
 }
 
