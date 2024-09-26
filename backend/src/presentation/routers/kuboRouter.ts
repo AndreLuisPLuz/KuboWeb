c366ba17d22c65b2a457663e218518696f3bc3f6
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-26 10:43:13 -0300

    feat: protected kubo routes with authentication.

--- backend/src/presentation/routers/kuboRouter.ts
+++ backend/src/presentation/routers/kuboRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { PRES_TOKENS, presentationContainer } from "../container";
+import authenticate from "../middlewares/authMiddleware";
 
 const kuboController = presentationContainer.get(PRES_TOKENS.kuboController);
 
 const kuboRouter = Router();
+kuboRouter.use(authenticate);
 kuboRouter.post("/cosmetic", kuboController.CreateCosmeticOption);
 
 export default kuboRouter;
