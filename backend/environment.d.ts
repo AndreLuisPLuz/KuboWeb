9152ff5db1ea35558ead5aefeda9018f8da66151
AndreLuisPLuz <andreluispluz@gmail.com>
2024-10-04 15:50:50 -0300

    chore: update environment variables to include a websocket port.

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -2,11 +2,15 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: "development" | "production",
+
             DB_HOST: "localhost" | string,
             DB_PORT: "27017" | string,
             DB_NAME: string,
+
             APP_PORT: string,
-            APP_SECRET_KEY: string
+            APP_SECRET_KEY: string,
+
+            SOCKET_PORT: string,
         }
     }
 }
