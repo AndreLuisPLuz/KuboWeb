f117a5073335ffaad8175f835f44585b71e73917
AndreLuisPLuz <andreluispluz@gmail.com>
2024-10-29 10:36:18 -0300

    feat: integration with vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights/>
     <BrowserRouter>
       <App />
     </BrowserRouter>
