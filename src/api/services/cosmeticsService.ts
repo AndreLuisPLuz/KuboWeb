978b07fc23fb1ecef91c5e5c5da38d7daddf3311
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-13 09:58:53 -0300

    feat: authorization on fetching of cosmetics.

--- src/api/services/cosmeticsService.ts
+++ src/api/services/cosmeticsService.ts
@@ -3,11 +3,18 @@ import { CosmeticsList } from "../types/cosmetics/CosmeticResponses";
 import { PaginationOptions } from "../types/shared/paginationOptions";
 
 const fetchCosmeticsService = async(
-        pagination: PaginationOptions): Promise<CosmeticsList> => {
+        pagination: PaginationOptions,
+        authToken: string
+)       : Promise<CosmeticsList> => {
     const { page, size } = pagination;
 
     const response = await kuboApi.get<CosmeticsList>(
-        `/kubo/cosmetic?page=${page}&size=${size}`
+        `/kubo/cosmetic?page=${page}&size=${size}`,
+        {
+            headers: {
+                Authorization: "Bearer " + authToken
+            }
+        }
     );
 
     return response.data;
