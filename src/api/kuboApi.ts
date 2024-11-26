b763c683193d7afcbb27be5a02af8e8a9e817a1c
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-26 08:43:31 -0300

    build: configured new build option for production environment, api url target now dependant on enviroment configuration.

--- src/api/kuboApi.ts
+++ src/api/kuboApi.ts
@@ -1,7 +1,16 @@
 import axios from "axios";
+import { match } from "ts-pattern";
+
+const baseUrl = match(process.env.NODE_ENV)
+    .with("development", () => "http://localhost:8080/api/v1")
+    .with("production", () => "https://kuboback.onrender.com/api/v1")
+    .otherwise(() => {
+        console.log("[server]: cannot access environment configuration.");
+        throw new Error("Killing execution");
+    });
 
 const kuboApi = axios.create({
-    baseURL: "https://kuboback.onrender.com/api/v1"
+    baseURL: baseUrl
 });
 
 export default kuboApi;
